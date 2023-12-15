8294fe69f0fe43c183f9e479b350b89dac6bba5f
lijiaqi <jiaqi.li@ienyan.com>
2023-12-15 16:02:44 +0800

    feat: 🎸 init

--- src/providersRPC.ts
+++ src/providersRPC.ts
@@ -1,42 +1,29 @@
-import { CHAINS } from '@lido-sdk/constants';
-import {
-  JsonRpcProvider,
-  JsonRpcBatchProvider,
-  StaticJsonRpcProvider,
-} from '@ethersproject/providers';
-import { StaticJsonRpcBatchProvider } from './staticJsonRpcBatchProvider';
+import { CHAINS } from '@lynx-sdk/constants'
+import { JsonRpcProvider, JsonRpcBatchProvider, StaticJsonRpcProvider } from '@ethersproject/providers'
+import { StaticJsonRpcBatchProvider } from './staticJsonRpcBatchProvider'
 
-const createProviderGetter = <P extends typeof JsonRpcProvider>(
-  Provider: P,
-) => {
-  const cache = new Map<string, InstanceType<P>>();
+const createProviderGetter = <P extends typeof JsonRpcProvider>(Provider: P) => {
+  const cache = new Map<string, InstanceType<P>>()
 
-  return (
-    chainId: CHAINS,
-    url: string,
-    cacheSeed = 0,
-    pollingInterval: number | null = null,
-  ): InstanceType<P> => {
-    const cacheKey = `${chainId}-${cacheSeed}-${url}`;
-    let provider = cache.get(cacheKey);
+  return (chainId: CHAINS, url: string, cacheSeed = 0, pollingInterval: number | null = null): InstanceType<P> => {
+    const cacheKey = `${chainId}-${cacheSeed}-${url}`
+    let provider = cache.get(cacheKey)
 
     if (!provider) {
-      provider = new Provider(url, chainId) as InstanceType<P>;
-      cache.set(cacheKey, provider);
+      provider = new Provider(url, chainId) as InstanceType<P>
+      cache.set(cacheKey, provider)
     }
 
     if (pollingInterval) {
-      provider.pollingInterval = pollingInterval;
+      provider.pollingInterval = pollingInterval
     }
 
-    return provider;
-  };
-};
+    return provider
+  }
+}
 
-export const getRpcProvider = createProviderGetter(JsonRpcProvider);
-export const getRpcBatchProvider = createProviderGetter(JsonRpcBatchProvider);
+export const getRpcProvider = createProviderGetter(JsonRpcProvider)
+export const getRpcBatchProvider = createProviderGetter(JsonRpcBatchProvider)
 
-export const getStaticRpcProvider = createProviderGetter(StaticJsonRpcProvider);
-export const getStaticRpcBatchProvider = createProviderGetter(
-  StaticJsonRpcBatchProvider,
-);
+export const getStaticRpcProvider = createProviderGetter(StaticJsonRpcProvider)
+export const getStaticRpcBatchProvider = createProviderGetter(StaticJsonRpcBatchProvider)
