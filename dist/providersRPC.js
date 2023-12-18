3b20b5d759982a724d19d3f088d5f13a95936202
lijiaqi <jiaqi.li@ienyan.com>
2023-12-18 22:25:43 +0800

    init

--- dist/providersRPC.js
+++ dist/providersRPC.js
@@ -1,26 +1,29 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getStaticRpcBatchProvider = exports.getStaticRpcProvider = exports.getRpcBatchProvider = exports.getRpcProvider = void 0;
-var providers_1 = require("@ethersproject/providers");
-var staticJsonRpcBatchProvider_1 = require("./staticJsonRpcBatchProvider");
-var createProviderGetter = function (Provider) {
-    var cache = new Map();
-    return function (chainId, url, cacheSeed, pollingInterval) {
-        if (cacheSeed === void 0) { cacheSeed = 0; }
-        if (pollingInterval === void 0) { pollingInterval = null; }
-        var cacheKey = "".concat(chainId, "-").concat(cacheSeed, "-").concat(url);
-        var provider = cache.get(cacheKey);
-        if (!provider) {
-            provider = new Provider(url, chainId);
-            cache.set(cacheKey, provider);
-        }
-        if (pollingInterval) {
-            provider.pollingInterval = pollingInterval;
-        }
-        return provider;
-    };
+'use strict';
+
+var providers = require('@ethersproject/providers');
+var staticJsonRpcBatchProvider = require('./staticJsonRpcBatchProvider.js');
+
+const createProviderGetter = Provider => {
+  const cache = new Map();
+  return (chainId, url, cacheSeed = 0, pollingInterval = null) => {
+    const cacheKey = `${chainId}-${cacheSeed}-${url}`;
+    let provider = cache.get(cacheKey);
+    if (!provider) {
+      provider = new Provider(url, chainId);
+      cache.set(cacheKey, provider);
+    }
+    if (pollingInterval) {
+      provider.pollingInterval = pollingInterval;
+    }
+    return provider;
+  };
 };
-exports.getRpcProvider = createProviderGetter(providers_1.JsonRpcProvider);
-exports.getRpcBatchProvider = createProviderGetter(providers_1.JsonRpcBatchProvider);
-exports.getStaticRpcProvider = createProviderGetter(providers_1.StaticJsonRpcProvider);
-exports.getStaticRpcBatchProvider = createProviderGetter(staticJsonRpcBatchProvider_1.StaticJsonRpcBatchProvider);
+const getRpcProvider = createProviderGetter(providers.JsonRpcProvider);
+const getRpcBatchProvider = createProviderGetter(providers.JsonRpcBatchProvider);
+const getStaticRpcProvider = createProviderGetter(providers.StaticJsonRpcProvider);
+const getStaticRpcBatchProvider = createProviderGetter(staticJsonRpcBatchProvider.StaticJsonRpcBatchProvider);
+
+exports.getRpcBatchProvider = getRpcBatchProvider;
+exports.getRpcProvider = getRpcProvider;
+exports.getStaticRpcBatchProvider = getStaticRpcBatchProvider;
+exports.getStaticRpcProvider = getStaticRpcProvider;
